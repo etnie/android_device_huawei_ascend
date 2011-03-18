750abfd2c73d0180c914c8312ec4cf6342bc9eb8
isaacj87 <icarusmod@gmail.com>
2011-03-18 10:26:17 -0500

    make sure our new libgralloc gets included in the build process

--- Android.mk
+++ Android.mk
@@ -12,5 +12,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-include $(call all-named-subdir-makefiles, libaudio libcopybit)
+include $(call all-named-subdir-makefiles, libaudio libcopybit libgralloc)
  
